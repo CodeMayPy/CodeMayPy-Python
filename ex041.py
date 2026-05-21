bd7c5c79bce17699ab9b6bce3906ccbde8659aca
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-05-20 22:46:51 -0300

    Revisão dos exercícios (if-elif)

--- ex041.py
+++ ex041.py
@@ -1,27 +1,5 @@
-#Ranking Athletes
-#41-The National Swimming Confederation needs a program that reads an athlete's birth year and shows their
-# category, according to age: – Up to 9 years old: child
-#– Up to 14 years old: children's
-#– Up to 19 years old: JUNIOR
-#– Up to 25 years old: SENIOR
-#– Over 25 years old: MASTER
-from datetime import date
-atual = date.today().year
-birth = int(input('What year was the athlete born?'))
-age = atual - birth
-print(F'the athlete was born in {birth} and has {age} years old.')
-if age <= 9:
-    print('Your category is CHILD!')
-if age >= 10 and age < 14:
-    print("Your category is CHILDREN'S.")
-if age >= 15 and age <= 19:
-    print('Your category is JUNIOR!')
-if age >= 20 and age <= 25:
-    print('Your category is SENIOR!')
-elif age >=26:
-    print('Your category is MASTER!')
-
-
+'''
+# Versão antiga
 #Classificando Atletas
 #041: A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre
 #sua categoria, de acordo com a idade:
@@ -45,6 +23,24 @@ if idade > 19 and idade <= 25:
 elif idade >= 26:
     print(f'Sua idade é {idade} e sua categoria é a master.')
 
+'''
 
-#PT- Condições aninhadas.
-#EN- Aligned conditions.
+# Versão revisada
+
+from datetime import date 
+
+ano_nascimento = int(input('Digite o ano do seu nascimento com 4 digitos: '))
+ano_atual = date.today().year
+
+idade = ano_atual - ano_nascimento
+
+if idade <= 9:
+    print(f'Você tem {idade} anos, sua categoria é a MIRIM.')
+elif idade > 9 and idade <= 14:
+    print(f'Você tem {idade} anos, sua categoria é a INFANTIL.')
+elif idade > 14 and idade <= 19:
+    print(f'Você tem {idade} anos, sua categoria é a JÚNIOR.')
+elif idade > 19 and idade <= 25:
+    print(f'Você tem {idade} anos, sua categoria é a SÊNIOR.')
+else:
+    print(f'Você tem {idade} anos, sua categoria é a MASTER.')
