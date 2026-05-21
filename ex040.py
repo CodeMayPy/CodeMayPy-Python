be0b65f33cbe12e657c830d73cb1b867d67f0aba
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-05-20 22:45:34 -0300

    Revisão dos exercícios (if-elif)

--- ex040.py
+++ ex040.py
@@ -1,20 +1,5 @@
-#That Average classic.
-#40- Create a program that reads two grades from a student and calculates their average, displaying a message at the end, according to the acquired media:
-# – Media below 5.0: FAILED
-# – AVERAGE BETWEEN 5.0 and 6.9: RECOVERED
-# – Media 7.0 or higher: PASSED
-first_note = float(input('Enter your note:'))
-second_note = float(input('Enter the second note:'))
-grade = ( first_note + second_note ) / 2
-print(f'Your avarege is {grade}.')
-if grade < 5.0:
-    print('You are FAILED!')
-if grade > 5.0:
-    print('You are RECOVERED!')
-if grade >= 6.9:
-    print('Congratulations you PASSED!')
-
-
+'''
+# versão antiga
 #Aquele clássico da Média.
 # 040: Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo
 # com a média atingida:
@@ -30,7 +15,18 @@ if media >= 5.0 and media < 6.9:
     print('Você está de recuperação.')
 if media >= 7.0:
     print('Parabéns você passou.')
+'''
 
+# Versão revisada
 
-#PT- Condições aninhadas.
-#EN- Aligned conditions.
+primeira_nota = float(input('Digite sua primeira nota: '))
+segunda_nota = float(input('Digite sua segunda nota:'))
+
+media = (primeira_nota + segunda_nota) / 2
+
+if media < 5.0:
+    print(f'Sua média é de {media}, você está REPROVADO(A)!')
+elif media >= 5.0 and media <= 6.9:
+    print(f'Sua média é {media} te vejo na RECUPERAÇÃO!')
+else:
+    print(f'Parabéns, sua média é de {media} você está APROVADO(A)!') 
