ccf91a612792c3f168c58e2463d1b941a955c552
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-05-20 22:38:32 -0300

    Revisão dos exercícios

--- ex038.py
+++ ex038.py
@@ -1,18 +1,5 @@
-#Comparing numbers
-#38- Write a program that reads two integers and compares them, displaying a message on the screen:
-# – The first value is greater
-# – The second value is greater
-# – There is no greater value, both are equal
-first_number = int(input('Enter an integer:'))
-second_number = int(input('Enter other integer:'))
-if first_number > second_number:
-    print(f'The number {first_number} is greater than {second_number}.')
-elif first_number < second_number:
-    print(f'The number {second_number} is greater than {first_number}.')
-else:
-    print('The two numbers are the same!')
-
-
+'''
+# Versão antiga
 #Comparando números
 #38: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
 #O primeiro valor é maior
@@ -26,6 +13,16 @@ elif segundo > primeiro:
     print(f'O número {segundo} é maior que {primeiro}.')
 else:
     print('Os dois números são iguais.')
+'''
 
-#PT- Condições aninhadas.
-#EN- Aligned conditions.
+# Versão revisada
+primeiro_numero = int(input('Digite um número inteiro: '))
+segundo_numero = int(input('Digite outro número inteiro: '))
+print(f'Vou comparar os números {primeiro_numero} e {segundo_numero} informados...')
+
+if primeiro_numero > segundo_numero:
+    print(f'O número {primeiro_numero} é maior que o {segundo_numero}.')
+elif primeiro_numero < segundo_numero:
+    print(f'O número {segundo_numero} é maior que {primeiro_numero}.')
+else:
+    print('Não existe número maior ou menor, os dois são iguais.')
