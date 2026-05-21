0f901cabd220294c3c520af877f53c3b8c4282cf
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-05-20 22:34:20 -0300

    Revisão dos exercícios

--- ex036.py
+++ ex036.py
@@ -1,22 +1,5 @@
-#Approving Loan
-#36-Write a program to approve a bank loan for the purchase of a house. Ask the buyer for the value of the house, the
-# buyer's salary and in how many years he will pay it off. The monthly payment cannot exceed 30% of the salary or
-# else the loan will be denied.
-
-'''house = float(input('What is the value of the house you want to buy in R$:'))
-salary = float(input('What is your salary in R$:'))
-time = float(input('How many years do you want to pay off the house?'))
-installment = house / (time * 12)
-condition = salary * 0.30
-print(f'To pay for a house R${house:.2f} in {time:.0f} years.')
-print(f'The installment will be of R${installment:.2f}.')
-if installment <= condition:
-    print('Congratulations your loan was approved!')
-else:
-    print('Try adding one more income.')'''
-
-
-#Aprovando Empréstimo
+''' 
+#Versão antiga.
 #36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do
 # comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
 casa = float(input('Qual o valor da casa que deseja comprar?'))
@@ -28,6 +11,18 @@ if parcela <= condicao:
     print('Empréstimo liberado.')
 else:
     print('Tente adicionar mais uma renda')
+'''
 
-#PT- Condições aninhadas.
-#EN- Aligned conditions.
+#versão revisada
+casa = float(input('Digite o valor da casa que deseja comprar R$: '))
+salario = float(input('Digite o valor do seu salário mensal: '))
+tempo = int(input('Em quantos anos você deseja pagar? '))
+
+meses = tempo * 12
+parcela = casa / meses
+limite = salario * 0.30
+
+if parcela <= salario * 0.30:
+    print(f'Seu empréstimo foi aprovado, e a parcela é de {parcela:.2f}')
+else:
+    print('Empréstimo negado, adicione mais renda.')
