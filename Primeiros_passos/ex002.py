21a171771a33f81a75c44fd56d6b51c555b8f06e
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 16:53:14 -0300

    refactor(ex002): limpa estrutura do codigo

--- Primeiros_passos/ex002.py
+++ Primeiros_passos/ex002.py
@@ -1,12 +1,2 @@
-#responding to user.
-#02- Create a program that asks for the user's name and writes a welcome message.
-name = input('What is your name:')
-print(f'Nice meet you, {name}!')
-
-#respondendo ao usuário.
-'''PT- Crie um programa que pergunte o nome do usuário e mostre uma mensagem de boas vindas.
 nome = input('Qual o seu nome?')
-print(f'É um prazer te conhecer {nome}, sejá bem vindo ao mundo Python')'''
-
-#PT- Nesse exercício fizemos o uso de uma variável "nome" que recebe um valor e logo mostra o mesmo na tela.
-#EN- In this exercise we used a "name" variable that receives a value and then shows it on the screen.
+print(f'É um prazer te conhecer {nome}, sejá bem vindo(a) ao mundo Python')
