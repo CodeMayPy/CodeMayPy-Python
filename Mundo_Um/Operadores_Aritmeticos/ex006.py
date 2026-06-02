325e04a505293784377d74601fc05ae7b1c4b62d
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 17:37:24 -0300

    refactor(ex006):limpa estrutura do codigo

--- Mundo_Um/Operadores_Aritmeticos/ex006.py
+++ Mundo_Um/Operadores_Aritmeticos/ex006.py
@@ -1,12 +1,2 @@
-#Double, Triple, Square Root
-#06- Create an algorithm that reads a number and shows its double, triple and square root.
-number = int(input('Enter a number:'))
-print(f' If the number is {number},\n your double will be {number*2},\n your triple will be {number*3}\n and its square root will be {number**0.5}.')
-
-#Dobro, Triplo, Raiz Quadrada
-'''#6: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
-numero = int(input('Digite um número inteiro e eu irei analizar:'))
-print(f'Se o número foi {numero}, \n Seu dobro é {numero * 2}, \n Seu triplo é {numero * 3}, \n E sua raiz quadrada é {numero**(1/2)}.')'''
-
-#PT- mais um treino de operadores aritméticos.
-#EN- another training in arithmetic operators.
+numero = int(input('Digite um número inteiro e eu irei analizar: '))
+print(f'Se o número foi {numero}, \n Seu dobro é {numero * 2}, \n Seu triplo é {numero * 3}, \n E sua raiz quadrada é {numero**(1/2):.2f}.')
