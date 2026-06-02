2178af1a138be38083e670100a81389dc69df60e
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 17:44:02 -0300

    refactor(ex009):limpa estrutura do codigo

--- Mundo_Um/Operadores_Aritmeticos/ex009.py
+++ Mundo_Um/Operadores_Aritmeticos/ex009.py
@@ -1,24 +1,4 @@
-#multiplication table.
-#09- Write a program that reads any integer and displays its multiplication table on the screen.
-number = int(input('Enter a value:'))
-print('-=' * 20)
-print('Analyzing the reported value...')
-print('-=' * 20)
-print('Your multiplication table will be:')
-print('{} x 1 = {}.'.format(number, number*1))
-print('{} x 2 = {}.'.format(number, number*2))
-print('{} x 3 = {}.'.format(number, number*3))
-print('{} x 4 = {}.'.format(number, number*4))
-print('{} x 5 = {}.'.format(number, number*5))
-print('{} x 6 = {}.'.format(number, number*6))
-print('{} x 7 = {}.'.format(number, number*7))
-print('{} x 8 = {}.'.format(number, number*8))
-print('{} x 9 = {}.'.format(number, number*9))
-print('{} x 10 = {}.'.format(number, number*10))
-
-#tabuada.
-#9: Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
-'''numero = int(input('Digite um número inteiro e eu lhe mostrarei sua tabuada:'))
+numero = int(input('Digite um número inteiro e eu lhe mostrarei sua tabuada: '))
 print(f'A tabuada do número {numero}:')
 print(f'{numero} x 1 = {numero*1}')
 print(f'{numero} x 2 = {numero*2}')
@@ -29,7 +9,4 @@ print(f'{numero} x 6 = {numero*6}')
 print(f'{numero} x 7 = {numero*7}')
 print(f'{numero} x 8 = {numero*8}')
 print(f'{numero} x 9 = {numero*9}')
-print(f'{numero} x 10 = {numero*10}')'''
-
-#PT- prática do comando format e da multiplicação.
-#EN- practice of the format command and multiplication.
+print(f'{numero} x 10 = {numero*10}')
