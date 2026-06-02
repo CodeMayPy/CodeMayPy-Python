f6377197018fefd95002b34c7dd7c2e534053f64
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 16:50:53 -0300

    refactor(ex001): limpa estrutura do codigo

--- Primeiros_passos/ex001.py
+++ Primeiros_passos/ex001.py
@@ -1,10 +1,1 @@
-#getting everything ready.
-# 01- Create a program that writes "Hello world!" on the screen.
-print('\033[1;32;40m Hello, world!\033[m')
-
-#deixando tudo pronto..
-'''PT- Crie um programa que escreva Olá mundo! na tela.
-print(Olá, mundo!)'''
-
-#PT- print vai mostrar na telá a mensagem que você deseja.
-#En- print will show the menssage you want.
+print('Olá, mundo!')
