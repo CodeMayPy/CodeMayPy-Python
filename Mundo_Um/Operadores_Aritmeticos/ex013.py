4b413d1f7502ed1f173b3f237c456ceccad3976a
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 17:45:54 -0300

    refactor(ex013):limpa estrutura do codigo

--- Mundo_Um/Operadores_Aritmeticos/ex013.py
+++ Mundo_Um/Operadores_Aritmeticos/ex013.py
@@ -1,15 +1,3 @@
-#Salary adjustment
-#13- Make a program that reads an employee's salary and shows his new salary with a 15% increase.
-salary = float(input('Enter your salary in R$:'))
-print('Calculating you new salary with a 15% increase...')
-increase = salary + (salary * 0.15)
-print(f'Your new salary will be R${increase}.')
-
-#Reajuste Salarial.
-#13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
-'''salario = float(input('Digite o valor do seu salário em R$:'))
+salario = float(input('Digite o valor do seu salário em R$: '))
 aumento = salario * 0.15
-print(f'Seu salário de R${salario:.2f}, com aumento de 15% ficará R${salario+aumento:.2f}')'''
-
-#PT- mais um treino de operadores aritméticos.
-#EN- another training in arithmetic operators
+print(f'Seu salário de R${salario:.2f}, com aumento de 15% ficará R${salario+aumento:.2f}')
