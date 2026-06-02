45c0f3aa2a1bd3d3dc4dac499bafc0955d6f859b
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 17:39:07 -0300

    refactor(ex007):limpa estrutura do codigo

--- Mundo_Um/Operadores_Aritmeticos/ex007.py
+++ Mundo_Um/Operadores_Aritmeticos/ex007.py
@@ -1,15 +1,4 @@
-#Arithmetic Mean
-#07- Develop a program that reads a student's two grades, calculates and displays their average.
-first_note = float(input('Enter your first note:'))
-second_note = float(input('Enter your second note:'))
-print(f' If your first note is {first_note} and your second note is {second_note}.\n You avarege will be {(first_note+second_note)/2}.')
-
-#Média Aritmética.
-'''#7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 print('Vamos calcular sua média?')
-primeira_nota = float(input('Digite a primeira nota:'))
-segunda_nota = float(input('Digite a segunda nota:'))
-print(f'A soma das suas notas é {primeira_nota + segunda_nota} logo a sua média será de {(primeira_nota + segunda_nota)/2}')'''
-
-#PT- mais um treino de operadores aritméticos.
-#EN- another training in arithmetic operators
+primeira_nota = float(input('Digite a primeira nota: '))
+segunda_nota = float(input('Digite a segunda nota: '))
+print(f'A soma das suas notas é {primeira_nota + segunda_nota} logo a sua média será de {(primeira_nota + segunda_nota)/2}')
