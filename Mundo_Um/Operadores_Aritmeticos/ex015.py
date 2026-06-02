1588126b9f616580e964ba6ab53d58085b93ab16
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-02 17:43:06 -0300

    refactor(ex015):limpa estrutura do codigo

--- Mundo_Um/Operadores_Aritmeticos/ex015.py
+++ Mundo_Um/Operadores_Aritmeticos/ex015.py
@@ -3,3 +3,4 @@ dias = int(input('Quandos dias você ficou com o carro alugado? '))
 valor_km = kilometros * 0.15
 valor_dia = dias * 60
 print(f'Se você rodou {kilometros}km e ficou {dias} dias com o carro, logo você deverá pagar R${valor_dia + valor_km} de locação do carro.')
+
