411314aebccc213f9c80a56e6a6aaab8cc7a21d7
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:25:09 -0300

    refactor(ex062):limpa estrutura do codigo

--- Mundo_dois/While/ex062.py
+++ Mundo_dois/While/ex062.py
@@ -1,26 +1,16 @@
-#EN- Loop structure: While
-#Super Arithmetic Progression v3.0
-#62- Improve challenge 61 by asking the user if he wants to show some more terms. The program will exit when he says he
-# wants to show 0 terms.
-print("Arithmetic progression generator")
+print('Gerador de progressão aritmética:')
 print("-=-" * 20)
-first_term = int(input("Enter the first term:"))
-reason = int(input("Enter the reason of the arithmetic progression:"))
-term = first_term
-counter = 1
+primeiro_termo = int(input("Digite o primeiro termo:"))
+razao = int(input("Digite a razão da progressão aritmética: "))
+termo = primeiro_termo
+contador = 1
 total = 0
-more = 10
-while more != 0:
-    total = more + total
-    while counter <= total:
-        print(f"{term}", end=' ')
-        term += reason
-        counter += 1
-    print("Pause")
-    more = int(input("How many more terms do you want to show?"))
-
-
-#PT- Estrutura de repetição: While
-#Super Progressão Aritmética v3.0
-#62: Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará
-# quando ele disser que quer mostrar 0 termos.
+mais = 10
+while mais != 0:
+    total = mais + total
+    while contador <= total:
+        print(f"{termo}", end=' ')
+        termo += razao
+        contador += 1
+    print("Pausa")
+    mais = int(input("Quantos termos a mais você quer mostrar? "))
