ea1abccbdf7ebcca139174ebf4132567cae5dd0e
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 11:01:06 -0300

    refactor(ex021):limpa estrutura do codigo

--- Mundo_Um/Utilizando_modulos/ex021.py
+++ Mundo_Um/Utilizando_modulos/ex021.py
@@ -1,21 +1,6 @@
-#Playing an MP3
-#21- Write a Python program that opens and plays audio from an MP3 file.
 import pygame
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load('nana.mp3')
 pygame.mixer.music.play()
 pygame.quit()
-
-
-#Tocando um MP3
-#21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
-import pygame
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('nana.mp3')
-pygame.mixer.music.play()
-pygame.quit()
-
-#PT- Treinando o uso de modulos, no caso math.
-#EN- Training the use of modules, in this case math
