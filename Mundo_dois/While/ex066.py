871957b6f7edb70228fbafae84701a4412ae08dd
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:29:49 -0300

    refactor(ex066):limpa estrutura do codigo

--- Mundo_dois/While/ex066.py
+++ Mundo_dois/While/ex066.py
@@ -1,10 +1,7 @@
-# Create a program that reads integers from the keyboard. The program will only stop when the user enters the value 999,
-# which is the stopping condition. At the end, show how many numbers were entered and what the sum of them was
-# (disregarding the flag).
-amount = 0
+soma = 0
 while True:
-    number = int(input('Ender a number (enter 999 to stop):'))
-    if number == 999:
+    numero = int(input("Digite um número inteiro [999 para parar]: "))
+    if numero == 999:
         break
-    amount += number
-print(f"The sum of the numbers is {amount}")
+    soma += numero
+print(f"A soma dos números é {soma}")
