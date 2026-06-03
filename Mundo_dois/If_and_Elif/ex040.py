6bb5b5b1b7cc3a22e7626fa639ad111c5878bee2
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:49:16 -0300

    refactor(ex040):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex040.py
+++ Mundo_dois/If_and_Elif/ex040.py
@@ -1,26 +1,5 @@
-'''
-# versão antiga
-#Aquele clássico da Média.
-# 040: Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo
-# com a média atingida:
-# Média abaixo de 5.0: REPROVADO
-# Média entre 5.0 e 6.9: RECUPERAÇÃO
-# Média 7.0 ou superior: APROVADO
-nota = float(input('Digite sua primeira nota:'))
-segunda_nota = float(input('Digite sua segunda nota:'))
-media = (nota + segunda_nota) / 2
-if media < 5.0:
-    print('Você está reprovado.')
-if media >= 5.0 and media < 6.9:
-    print('Você está de recuperação.')
-if media >= 7.0:
-    print('Parabéns você passou.')
-'''
-
-# Versão revisada
-
 primeira_nota = float(input('Digite sua primeira nota: '))
-segunda_nota = float(input('Digite sua segunda nota:'))
+segunda_nota = float(input('Digite sua segunda nota: '))
 
 media = (primeira_nota + segunda_nota) / 2
 
