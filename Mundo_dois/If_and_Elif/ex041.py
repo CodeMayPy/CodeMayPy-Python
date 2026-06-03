aea67742c82ee577fa58df85778d09814cb178b0
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:49:46 -0300

    refactor(ex041):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex041.py
+++ Mundo_dois/If_and_Elif/ex041.py
@@ -1,32 +1,3 @@
-'''
-# Versão antiga
-#Classificando Atletas
-#041: A Confederação Nacional de Natação precisa de um programa que leia o ano de nascimento de um atleta e mostre
-#sua categoria, de acordo com a idade:
-# Até 9 anos: MIRIM
-# Até 14 anos: INFANTIL
-# Até 19 anos: JÚNIOR
-# Até 25 anos: SÊNIOR
-# Acima de 25 anos: MASTER
-from datetime import date
-nascimento = int(input('Qual o seu ano de nascimento?'))
-ano = date.today().year
-idade = ano - nascimento
-if idade <= 9:
-    print(f'Sua idade é {idade} então sua categoria é a mirim.')
-if idade > 9 and idade <= 14:
-    print(f'Sua idade é {idade} então sua categoria é a infantil.')
-if idade  > 14 and idade <= 19:
-    print(f'Sua idade é {idade} então sua categoria é a junior.')
-if idade > 19 and idade <= 25:
-    print(f'Sua idade é {idade} então sua categoria é a sênior.')
-elif idade >= 26:
-    print(f'Sua idade é {idade} e sua categoria é a master.')
-
-'''
-
-# Versão revisada
-
 from datetime import date 
 
 ano_nascimento = int(input('Digite o ano do seu nascimento com 4 digitos: '))
