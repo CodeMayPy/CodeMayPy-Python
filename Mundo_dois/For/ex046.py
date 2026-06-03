ac37fcf973b8f9b4f45d09e58ede952b5c8c2088
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:55:46 -0300

    refactor(ex046):limpa estrutura do codigo

--- Mundo_dois/For/ex046.py
+++ Mundo_dois/For/ex046.py
@@ -1,22 +1,5 @@
-#Countdown
-#46- Make a program that displays a countdown on the screen for fireworks to go off, going from 10 to 0,
-# with a 1 second pause between them.
-from time import sleep
-for c in range (10, -1, -1):
-    print(c)
-    sleep(1)
-print('Bum! Bum! POOOOW!')
-
-
-#Contagem regressiva
-# 46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até
-# 0, com uma pausa de 1 segundo entre eles.
 from time import sleep
 for contador in range(10,-1,-1):
     print(contador)
     sleep(1)
 print('Bum, bum, poooww!')
-
-
-#PT- Estruturas de repetições: For.
-#EN- Loop structures: For.
