9e5b88a5bbc8c5b6068bdd2cdce1c900d77b589f
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:35:54 -0300

    refactor(ex069):limpa estrutura do codigo

--- Mundo_dois/While/ex069.py
+++ Mundo_dois/While/ex069.py
@@ -1,14 +1,9 @@
-# 069: Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá
-# perguntar se o usuário quer ou não continuar. No final, mostre:
-# A) quantas pessoas tem mais de 18 anos.
-# B) quantos homens foram cadastrados.
-# C) quantas mulheres tem menos de 20 anos.
 total_maior = total_homem = total_mulher = 0
 while True:
-    idade = int(input('Qual a sua idade?'))
+    idade = int(input('Qual a sua idade? '))
     genero = ' '
     while genero not in "FM":
-        genero = str(input('Qual o seu gênero? [F/M]')).strip().upper()[0]
+        genero = str(input('Qual o seu gênero? [F/M] ')).strip().upper()[0]
     if idade >= 18:
         total_maior += 1
     if genero == "M":
@@ -17,7 +12,7 @@ while True:
         total_mulher += 1
     resposta = ' '
     while resposta not in "SN":
-        resposta = str(input('Deseja continuar? [S/N]')).strip().upper()[0]
+        resposta = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
     if resposta == "N":
         break
 print(f"O total de pessoas com mais de 20 anos é {total_maior}.")
