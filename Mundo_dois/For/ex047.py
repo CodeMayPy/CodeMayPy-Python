d02c4d8af458e07e7d91a8950f1f63a8374899ba
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:56:28 -0300

    refactor(ex047):limpa estrutura do codigo

--- Mundo_dois/For/ex047.py
+++ Mundo_dois/For/ex047.py
@@ -1,15 +1,2 @@
-#Pair Count
-#47- Create a program that displays on the screen all even numbers that are in the range between 1 and 50.
-for c in range (2, 51,2):
-    print(c, end=' ')
-print("Finished!")
-
-
-#Contagem de pares
-# 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 for contador in range(2,51,2):
     print(contador, end=' ')
-
-
-#PT- Estruturas de repetições: For.
-#EN- Loop structures: For.
