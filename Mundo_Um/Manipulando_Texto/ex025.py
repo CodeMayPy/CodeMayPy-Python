eb826924a9a82d9ef546fe3482c22a13c2c6b3a4
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 11:06:43 -0300

    refactor(ex025):limpa estrutura do codigo

--- Mundo_Um/Manipulando_Texto/ex025.py
+++ Mundo_Um/Manipulando_Texto/ex025.py
@@ -1,12 +1,2 @@
-#Searching for a string within another
-#25- Create a program that reads a person's name and tells if they have "Silva" in their name
-name = str(input('What your full name?'))
-print(F'Your name has Silva? {'silva' in name.lower()}.')
-
-#Procurando uma string dentro de outra
-#25: Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
-'''nome = str(input('Qual é o seu nome completo?'))
-print(f'No seu nome tem Silva? {'silva' in name.lower()}')'''
-
-#PT- treinando a manipulação de strings.
-#EN- training string manipulation.
+nome = str(input('Qual é o seu nome completo? '))
+print(f'No seu nome tem Silva? {'silva' in nome.lower()}')
