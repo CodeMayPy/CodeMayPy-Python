de7730d19dfaead60f1d6bfcdeee69deded64920
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:27:46 -0300

    refactor(ex064):limpa estrutura do codigo

--- Mundo_dois/While/ex064.py
+++ Mundo_dois/While/ex064.py
@@ -1,11 +1,7 @@
-#EN- Loop structure: While
-#64-Create a program that reads several integers from the keyboard. The program will only stop when the user enters the
-# value 999, which is the stopping condition. At the end, show how many numbers were entered and what the sum of them
-# was (disregarding the flag).
-number = counter = amount = 0
-number = int(input("Enter an integer [999 to stop]:"))
-while number != 999:
-    amount += number
-    counter += 1
-    number = int(input("Enter an integer [999 to stop]:"))
-print(f"You enter {number} numbers and the sum between them is {amount} ")
+numero = contador = soma = 0
+numero = int(input("Digite um número inteiro [999 para parar]: "))
+while numero != 999:
+    soma += numero
+    contador += 1
+    numero = int(input("Digite um número inteiro [999 para parar]:"))
+print(f"Você digitou {contador} números e a soma entre eles é {soma} ") 
