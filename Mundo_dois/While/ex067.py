a004f11c451fe6e8fcef5d6bd5cfd5d36787e89b
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:30:45 -0300

    refactor(ex067):limpa estrutura do codigo

--- Mundo_dois/While/ex067.py
+++ Mundo_dois/While/ex067.py
@@ -1,8 +1,6 @@
-# Write a program that displays the multiplication table of several numbers, one at a time, for each value entered by
-# the user. The program will stop when the requested number is negative.
 while True:
-    number = int(input("What number do you want to see the multiplication of?"))
-    if number < 0:
+    numero = int(input("Digite um número para ver a tabuada: "))
+    if numero < 0:
         break
-    for counter in range (1,11):
-        print(f'{number} X {counter} = {number * counter}')
+    for contador in range(1, 11):
+        print(f"{numero} X {contador} = {numero * contador}")
