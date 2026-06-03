5bbca34972f7fad38c19c74a4047287e7851a4a5
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 11:03:36 -0300

    refactor(ex022):limpa estrutura do codigo

--- Mundo_Um/Manipulando_Texto/ex022.py
+++ Mundo_Um/Manipulando_Texto/ex022.py
@@ -1,28 +1,6 @@
-#Text Analyzer
-#22- Create a program that reads a person's full name and displays:
-# - The name in all upper and lower case letters.
-# - How many letters in total without considering spaces
-# - How many letters are in the first name.
-name = str(input('Enter your full name:'))
-print('Analyzing your name...')
-print(f'Your name in capital letters:{name.upper()}.')
-print(f'Your name in lower case:{name.lower()}.')
-print(f'Your name in total has {len(name)-name.count(' ')} letters.')
-separate = name.split()
-print(f'Your first name is {separate[0]} and he has {len(separate[0])} letters.')
-
-#Analisador de Textos
-# 022: Crie um programa que leia o nome completo de uma pessoa e mostre:
-# O nome com todas as letras maiúsculas e minúsculas.
-# Quantas letras ao todo (sem considerar espaços).
-# Quantas letras tem o primeiro nome.
-'''nome = str(input("Qual o seu nome completo?"))
+nome = str(input("Qual o seu nome completo? "))
 print(f"Seu nome com todas as letras em maiúsculo fica: {nome.upper()}")
 print(f"Seu nome em minúsculo fica: {nome.lower()}")
 print(f"Seu nome sem espaços tem {len(nome)-nome.count(' ')} letras.")
 separado = nome.split()
-print(f"Seu primeiro nome {separado[0]}  tem {len(separado[0])} letras.")'''
-
-
-#PT- treinando a manipulação de strings.
-#EN- training string manipulation.
+print(f"Seu primeiro nome {separado[0]}  tem {len(separado[0])} letras.")
