ab4e97028fb5e7eda6e2b1bb5fc289cfc4233115
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:52:07 -0300

    refactor(ex072): revisa e melhora codigo

--- Mundo_tres/Tuplas/ex072.py
+++ Mundo_tres/Tuplas/ex072.py
@@ -1,13 +1,11 @@
-# 72-Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte. Seu
-# programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
-contagem = ('zero', 'um', 'dois', 'três', 'quatro',
-            'cinco', 'seis', 'sete', 'oito', 'nove',
-            'dez', 'onze', 'doze', 'treze', 'quatorze',
-            'quinze', 'dezesseis', 'dezessete', 'dezoito',
-            'dezenove', 'vinte')
+contagem = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nova', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
+
 while True:
-    numero = int(input('Digite um número de 0 a 20:'))
+    numero = int(input('Digite um número de 0 a 20: '))
+
     if 0 <= numero <= 20:
+        print(f'O número informado foi: {contagem[numero]}.')
         break
-    print('Tente novamente.' ,end=' ')
-print(f"Seu número é {contagem[numero]}."
+
+    else:
+        print('Tente novamente.')
