3017303e2fee1b532d2737363fb8f367694417cd
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:28:45 -0300

    refactor(ex065):limpa estrutura do codigo

--- Mundo_dois/While/ex065.py
+++ Mundo_dois/While/ex065.py
@@ -1,21 +1,17 @@
-#EN- Loop structure: While
-# 65- Create a program that reads several integers from the keyboard. At the end of the execution, show the average of
-# all the values and which were the highest and lowest values read. The program should ask the user whether or not he
-# wants to continue entering values.
-response = "Y"
-medium = soma = amount = bigger = smaller = 0
-while response in "Yy":
-    number = int(input("Enter a number:"))
-    soma += number
-    amount += 1
-    if amount == 1:
-        bigger = smaller = number
+resposta = "S"
+media = soma = quantidade = maior = menor = 0
+while resposta in "Ss":
+    numero = int(input("Digite um número:"))
+    soma += numero
+    quantidade += 1
+    if quantidade == 1:
+        maior = menor = numero
     else:
-        if number > bigger:
-            bigger = number
-        if number < smaller:
-            smaller = number
-    response = str(input("Do you want to continue?")).upper().strip()[0]
-medium = soma / number
-print(f"You entered {amount} and the averege is {medium}.")
-print(f"The bigger is {bigger} and the smaller is {smaller}.")
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+    resposta = str(input("Quer continuar?")).upper().strip()[0]
+media = soma / quantidade
+print(f"Você digitou {quantidade} e a média é {media}.")
+print(f"O maior é {maior} e o menor é {menor}.")
