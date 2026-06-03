ff3b6157a4514d86db31312b8fa2a8addad5e4d0
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:57:51 -0300

    refactor(ex049):limpa estrutura do codigo

--- Mundo_dois/For/ex049.py
+++ Mundo_dois/For/ex049.py
@@ -1,16 +1,3 @@
-#Multiplication Table v.2.0
-#49- Redo CHALLENGE 9, showing the multiplication table of a number that the user chooses, but now using a for loop.
-number = int(input('Enter a number to showing the multiplication:'))
-for c in range (1,11):
-    print(f'{number} x {c:2} = {number * c}.')
-
-
-#Tabuada v.2.0
-# 49: Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for.
-numero = int(input('Digite o número que você quer saber a tabuada:'))
+numero = int(input('Digite o número que você quer saber da tabuada: '))
 for c in range(1,11):
     print(f'{numero} x {c:2} = {numero *c}')
-
-
-# PT- Estruturas de repetições: For.
-# EN- Loop structures: For.
