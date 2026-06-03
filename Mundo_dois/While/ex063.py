cb4c8e7f6944012959bfa66779d6a8bb38fdd84f
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:26:42 -0300

    refactor(ex063):limpa estrutura do codigo

--- Mundo_dois/While/ex063.py
+++ Mundo_dois/While/ex063.py
@@ -1,14 +1,11 @@
-#EN- Loop structure: While
-#63-Write a program that reads any integer number N and displays on the screen the first N elements of a
-# Fibonacci Sequence.
-terms = int(input("How many terms do you want to show?"))
-first_term = 0
-second_term = 1
-print(f"{first_term} -> {second_term}", end='')
-counter = 3
-while counter <= terms:
-    third_term = first_term + second_term
-    print(f" -> {third_term}", end='')
-    first_term = second_term
-    second_term = third_term
-    counter += 1
+termos = int(input("Quantos termos você quer mostrar? "))
+primeiro_termo = 0
+segundo_termo = 1
+print(f"{primeiro_termo} -> {segundo_termo}", end=' ')
+contador = 3
+while contador <= termos:
+    terceiro_termo = primeiro_termo + segundo_termo
+    print(f" -> {terceiro_termo}", end='')
+    primeiro_termo = segundo_termo
+    segundo_termo = terceiro_termo
+    contador += 1
