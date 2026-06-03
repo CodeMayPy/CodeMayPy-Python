67e9e1fef425fff634c8d763fb69f4d1fd03974d
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 17:53:53 -0300

    refactor(ex075): revisa e melhora codigo

--- Mundo_tres/Tuplas/ex075.py
+++ Mundo_tres/Tuplas/ex075.py
@@ -1,17 +1,26 @@
-#075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
-# A) Quantas vezes apareceu o valor 9.
-# B) Em que posição foi digitado o primeiro valor 3.
-# C) Quais foram os números pares.
-numero = (int(input('Digite um número:')), int(input('Digite um número:')),
-          int(input('Digite um número:')), int(input('Digite um número:')))
-print(f"Você digitou os valores: {numero}")
-print(f'O valor 9 apareceu {numero.count(9)} vezes.')
-if 3 in numero:
-    print(f'O valor 3 apareceu na {numero.index(3)+1}ª posição.')
-else:
-    print('O valor 3 não foi digitado.')
-print(f'Os números pares foram:',end=' ')
-for n in numero:
-    if n % 2 == 0:
-        print(n, end=' ')
+lista = []
 
+for c in range(4):
+    numeros = int(input(f'Digite o {c+1}º número: '))
+    lista.append(numeros)
+
+numeros_tupla = tuple(lista)
+
+print(f'Sua tupla ficou assim: {numeros_tupla}.')
+print(f'O número 9 apareceu {numeros_tupla.count(9)} vezes.')
+
+if 3 in numeros_tupla:
+    print(f'O número 3 apareceu primeiro na posição {numeros_tupla.index(3)+1}º informada.')
+else:
+    print('O número 3 não foi digitado em nenhuma posição.')
+
+lista_par = []
+
+for par in numeros_tupla:
+    if par % 2 == 0:
+        lista_par.append(par)
+
+if lista_par:
+    print(f'Os números pares digitados foram: {tuple(lista_par)}.')
+else:
+    print('Nenhum número par foi digitado.')
