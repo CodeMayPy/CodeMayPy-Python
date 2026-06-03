01815071305d5152792b88a156e29e4e45ecb617
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:51:01 -0300

    refactor(ex043):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex043.py
+++ Mundo_dois/If_and_Elif/ex043.py
@@ -1,30 +1,3 @@
-'''
-#Versão antiga
-#Índice de Massa Corporal
-# 43: Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu Índice de Massa Corporal (IMC) e mostre
-#seu status, de acordo com a tabela abaixo:
-# IMC abaixo de 18,5: Abaixo do Peso
-# Entre 18,5 e 25: Peso Ideal
-# 25 até 30: Sobrepeso
-# 30 até 40: Obesidade
-# Acima de 40: Obesidade Mórbida
-peso = float(input('Qual é o seu peso em KG:'))
-altura = float(input('Qual é a sua altura em metros?'))
-imc = peso / (altura ** 2)
-if imc < 18.5:
-    print(f'Seu IMC é {imc:.2f} e você está abaixo do peso.')
-elif imc >= 18.5 and imc <25:
-    print(f'Seu IMC é {imc:.2f} e você está no seu peso ideal.')
-elif imc >= 25 and imc < 30:
-    print(f'Seu IMC é de {imc:.2f} e você está com sobrepeso.')
-elif imc >= 30 and imc <= 40:
-    print(f'Seu IMC é de {imc:.2f} e você está com obesidade.')
-elif imc > 40:
-    print(f'Seu IMC é de {imc:.2f} e você está com obesidade mórbida.')
-'''
-
-#Versão revisada
-
 print('Vamos ver se seu IMC está ideal...')
 peso = float(input('Digite seu peso KG: '))
 altura = float(input('Digite a sua altura: '))
