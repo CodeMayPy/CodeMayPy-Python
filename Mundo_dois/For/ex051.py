27882cfc2c04e93ad0d11f16ee5fe5a09416d109
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 14:02:39 -0300

    refactor(ex052):limpa estrutura do codigo

--- Mundo_dois/For/ex051.py
+++ Mundo_dois/For/ex051.py
@@ -1,24 +1,6 @@
-#Arithmetic Progression
-#51-Develop a program that reads the first term and the ratio of an AP. At the end, show the first 10 terms
-# of this progression.
-first = int(input('First term:'))
-ratio = int(input('Ratio:'))
-tenth = first + (10-1) * ratio
-for c in range(first,tenth + ratio, ratio):
-    print(f'{c}', end=' -> ')
-print('He finished')
-
-
-#Progressão Aritmética
-# 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos
-# dessa progressão.
-primeiro = int(input('Digite o primeiro termo da PA:'))
+primeiro = int(input('Digite o primeiro termo da PA: '))
 razao = int(input('Digite a razão da PA:'))
 dez = primeiro + (10 - 1) * razao
 for contador in range(primeiro, dez + razao, razao):
     print(f'{contador}', end='-')
 print('Fim')
-
-
-#PT- Estruturas de repetições: For.
-#EN- Loop structures: For.
--- Mundo_dois/For/ex052.py
+++ Mundo_dois/For/ex052.py
@@ -1,24 +1,4 @@
-#Prime numbers
-#52- Write a program that reads an integer and tells whether or not it is a prime number.
-number = int(input('Enter a integer number:'))
-tot = 0
-for c in range(1, number, +1):
-    if number % c == 0:
-        print('\033[33m', end=' ')
-        tot += 1
-    else:
-        print('\033[31m', end=' ')
-    print(f'{c}', end=' ')
-print(f'\n\033[m The number {number} was divisible {tot} times.')
-if tot == 2:
-    print('And that is why he is a prime number.')
-else:
-    print('And that is why it is not a prime number.')
-
-
-#Números primos
-#52: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
-numero = int(input('Digite um número:'))
+numero = int(input('Digite um número: '))
 total = 0
 for contador in range(1,numero,+1):
     if numero % contador == 0:
@@ -30,6 +10,3 @@ if total == 2:
     print('O número é primo.')
 else:
     print('O número é impar.')
-
-# PT- Estruturas de repetições: For.
-# EN- Loop structures: For.
