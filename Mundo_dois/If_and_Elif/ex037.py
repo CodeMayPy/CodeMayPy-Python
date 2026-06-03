9d98edd39605b07c5da12da80dfb3120e72bfbaf
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:47:38 -0300

    refactor(ex037):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex037.py
+++ Mundo_dois/If_and_Elif/ex037.py
@@ -1,24 +1,3 @@
-'''
-#Versão antiga
-#37: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de
-#conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
-numero = int(input('Digite um número inteiro qualquer:'))
-print('''Escolha uma das bases de conversão:
-      [ 1 ] BINÁRIO'
-      [ 2 ] OCTAL'
-      [ 3 ] HEXADECIMAL''')
-opcao = int(input('Digite sua escolha:'))
-if opcao == 1:
-    print(f'o número {numero} convertido para binário fica: {bin(numero)[2:]}.')
-elif opcao == 2:
-    print(f'O número {numero} convertido para octal fica: {oct(numero)[2:]}.')
-elif opcao == 3:
-    print(f'O número {numero} convertido para hexadecimal fica: {hex(numero)[2:]}.')
-else:
-    print('Opção inválida, tente novamente.')
-'''
-
-#Versão revisada
 numero = int(input('Digite um número inteiro qualquer: '))
 print(f'Perfeito, você digitou: {numero}.\nAgora vamos transformar ele, escolha uma opção:')
 
