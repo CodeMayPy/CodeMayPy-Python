9fcc3731225c0d9914129762cadf87f052b2cfc6
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 14:04:55 -0300

    refactor(ex054):limpa estrutura do codigo

--- Mundo_dois/For/ex054.py
+++ Mundo_dois/For/ex054.py
@@ -1,35 +1,12 @@
-#Majority Group
-#54: Create a program that reads the birth year of seven people. At the end, show how many people have not yet
-# the age of majority and how many are already adults.
-from datetime import date
-actual = date.today().year
-higthest_total = smaller_total= 0
-for people in range(1,8):
-    birth = int(input(f"What year was {people}ª person born?"))
-    age = actual - birth
-    if age >= 21:
-        higthest_total += 1
-    else:
-        smaller_total += 1
-print(f"In total we had {higthest_total} people of legal age.")
-print(f"and {smaller_total} people under age.")
-
-
-#Grupo da Maioridade
-#54- Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a
-# maioridade e quantas já são maiores.
 atual = date.today().year
-totmaior = 0
-totmenor = 0
+total_maior = 0
+total_menor = 0
 for pessoas in range(1, 8):
-    nas = int(input(f'Em que ano a {pessoas}ª pessoa nasceu?'))
-    idade = atual - nas
+    nascimento = int(input(f'Em que ano a {pessoas}ª pessoa nasceu?'))
+    idade = atual - nascimento
     if idade >= 21:
-        totmaior += 1
+        total_maior += 1
     else:
-        totmenor += 1
-print(f' Ao todo tivemos {totmaior} pessoas maiores de idade.')
-print(f'E {totmenor} pessoas menores de idade.')
-
-#PT- Estruturas de repetições: For.
-#EN- Loop structures: For.
+        total_menor += 1
+print(f' Ao todo tivemos {total_maior} pessoas maiores de idade.')
+print(f'E {total_menor} pessoas menores de idade.')
