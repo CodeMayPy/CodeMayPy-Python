48a5c372fbee50e4aab713266be21296bed6b8d5
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:48:09 -0300

    refactor(ex038):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex038.py
+++ Mundo_dois/If_and_Elif/ex038.py
@@ -1,22 +1,3 @@
-'''
-# Versão antiga
-#Comparando números
-#38: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
-#O primeiro valor é maior
-#O segundo valor é maior
-#Não existe valor maior, os dois são iguais
-primeiro = int(input('Digite um número:'))
-segundo = int(input('Digite outro número:'))
-if primeiro > segundo:
-    print(f'O número {primeriro} é maior que {segundo}.')
-elif segundo > primeiro:
-    print(f'O número {segundo} é maior que {primeiro}.')
-else:
-    print('Os dois números são iguais.')
-'''
-
-
-# Versão revisada
 primeiro_numero = int(input('Digite um número inteiro: '))
 segundo_numero = int(input('Digite outro número inteiro: '))
 print(f'Vou comparar os números {primeiro_numero} e {segundo_numero} informados...')
