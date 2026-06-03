ff89450b72417224ad198bcad386abb2265d8770
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:48:40 -0300

    refactor(ex039):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex039.py
+++ Mundo_dois/If_and_Elif/ex039.py
@@ -1,27 +1,6 @@
-'''
-#Versão antiga
-#Alistamento Militar
-#39: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se
-#alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também
-#deverá mostrar o tempo que falta ou que passou do prazo.
-from datetime import date
-ano_nascimento = int(input('Qual o seu ano de nascimento?'))
-ano = date.today().year
-idade = ano - ano_nascimento
-print(f'Se você nasceu em {ano_nascimento} a sua idade é {idade}.')
-if idade == 18:
-    print('Você deve se alistar.')
-elif idade < 18:
-    print(f'Você ainda tem {18 - idade} aos até se alistar.')
-elif idade > 18:
-    print(f'Já passou {idade - 18} do prazo de alistamento')
-'''
-
-
-# Versão revisada
 from datetime import date
 
-ano_nascimento = int(input('Digite o ano do seu nascimeto com 4 digitos:'))
+ano_nascimento = int(input('Digite o ano do seu nascimeto com 4 digitos: '))
 
 ano_atual = date.today().year
 idade = ano_atual - ano_nascimento
