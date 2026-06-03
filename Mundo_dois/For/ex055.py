2b6bc9c1248b746079a7c98df3102c872a4458bf
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 14:05:45 -0300

    refactor(ex055):limpa estrutura do codigo

--- Mundo_dois/For/ex055.py
+++ Mundo_dois/For/ex055.py
@@ -1,26 +1,7 @@
-#Largest and smallest of the sequence
-#55-Make a program that reads the weight of five people. At the end, show which was the largest and smallest weight read.
-bigger = smaller = 0
-for p in range(1, 6):
-    weight = float(input(f"What's the weight of person {p}"))
-    if p == 1:
-        bigger = weight
-        smaller = weight
-    else:
-        if weight > bigger:
-            bigger = weight
-            if weight < smaller:
-                smaller = weight
-print(f"The largest weight read was {bigger}kg")
-print(f"The smallest weight read was {smaller}Kg")
-
-
-#Maior e menor da sequência
-#55-Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 maior = 0
 menor = 0
 for p in range (1,6):
-    peso = float(input(f'Qual o pesso da {p}ª pessoa:'))
+    peso = float(input(f'Qual o pesso da {p}ª pessoa: '))
     if p == 1:
         maior = peso
         menor = peso
@@ -31,7 +12,3 @@ for p in range (1,6):
             menor = peso
 print(f'O maior peso lido foi de {maior}kg.')
 print(f' O menor peso lido foi de {menor}Kg,')
-
-
-#PT- Estruturas de repetições: For.
-#EN- Loop structures: For.
