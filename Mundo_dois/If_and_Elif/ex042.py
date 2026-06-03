031f6d1ea045f0a32d4af4ba47eefb25a1cb3212
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-03 13:50:30 -0300

    refactor(ex042):limpa estrutura do codigo

--- Mundo_dois/If_and_Elif/ex042.py
+++ Mundo_dois/If_and_Elif/ex042.py
@@ -1,29 +1,5 @@
-'''
-#Versão antiga
-#Analisando Triângulos v2.0
-#42: Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
-# EQUILÁTERO: todos os lados iguais
-# ISÓSCELES: dois lados iguais, um diferente
-# ESCALENO: todos os lados diferentes
-primeiro = float(input('Qual o primeiro valor?'))
-segundo = float(input('Qual o segundo valor?'))
-terceiro = float(input('Qual o terceiro valor?'))
-if primeiro < segundo + terceiro and segundo < primeiro + terceiro and terceiro < primeiro + segundo:
-    print('Os dadod informados podem formar um triângulo, ' ,end=' ')
-    if primeiro == segundo == terceiro:
-        print('Equilátero.')
-    elif primeiro != segundo != terceiro != primeiro:
-        print('Escaleno')
-    else:
-        print('isósceles.')
-else:
-    print('Os dados informados não formaram um triângulo.')
-    '''
-
-#Versão revisada
-
-primeiro_valor = float(input('Digite o valor da primeira reta:'))
-segundo_valor = float(input('Digite o valor da segunda reta:'))
+primeiro_valor = float(input('Digite o valor da primeira reta: '))
+segundo_valor = float(input('Digite o valor da segunda reta: '))
 terceiro_valor = float(input('Digite o valor da terceira reta:'))
 
 if primeiro_valor < segundo_valor + terceiro_valor and segundo_valor < primeiro_valor + terceiro_valor and terceiro_valor < primeiro_valor + segundo_valor: #valida a existência do triângulo
