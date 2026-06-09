cec3dc96aa8b5157c26630017cac34f179bfa9b5
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-06-09 15:31:13 -0300

    refactor(ex080): limpa estrutura codigo

--- Mundo_tres/Listas/ex080.py
+++ Mundo_tres/Listas/ex080.py
@@ -1,6 +1,6 @@
 lista = []
 for c in range (0,5):
-    numero = int(input('Digiter um valor:'))
+    numero = int(input('Digite um valor:'))
     if c == 0 or numero > lista[-1] :
         lista.append(numero)
     else:
